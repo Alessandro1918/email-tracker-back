a607ad774017c0aafc9fdb37f49e3453463dadfb
Alessandro1918 <alessandro1918@hotmail.com>
2023-08-01 16:56:28 -0300

    feat: route /track returning .png image

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express")
+const path = require("path")
 
 const PORT = process.env.PORT || 4000
 
 const app = express()
 
+app.use('/images', express.static('images'))
+
 //http://localhost:4000/track?id=1&recipient=2&subject=3
 app.get("/track", (req, res) => {
-  console.log(`ID: ${req.query["id"]}`)
-  console.log(`recipient: ${req.query["recipient"]}`)
-  console.log(`subject: ${req.query["subject"]}`)
-  return res.status(200).send("Hello, World!")
+  console.log(`New request! ID: ${req.query["id"]}  Recipient: ${req.query["recipient"]}  Subject: ${req.query["subject"]}`)
+  return res
+    .status(200)
+    // .send("Hello, World!")
+    .sendFile(path.join(__dirname, "images/pixel.png"))
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
